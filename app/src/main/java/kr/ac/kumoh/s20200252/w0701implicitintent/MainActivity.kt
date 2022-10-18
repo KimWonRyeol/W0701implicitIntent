57b91d422da21a36021e97a208b8aa009faedef6
Kimwonryeol <wonryeol3943@gmail.com>
2022-10-18 14:26:19 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20200252/w0701implicitintent/MainActivity.kt
@@ -0,0 +1,30 @@
+package kr.ac.kumoh.s20200252.w0701implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.s20200252.w0701implicitintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+
+        binding.btnHomepage.setOnClickListener {
+            val uri = Uri.parse("https://www.facebook.com")
+            val intent =  Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        binding.btnMap.setOnClickListener {
+            val uri = Uri.parse("geo: 0,0? z =17&q=장흥군 장흥읍 동교 3길 5")
+            val intent =  Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+    }
+}
